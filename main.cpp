45f49086b49575f64ee60a992d2c724ae5e2ee1d
Kurt Brice <kurtbrice@Kurts-MacBook-Pro.local>
2025-03-12 15:15:07 +1030

    6

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include "Human.h"
 #include "Referee.h"
+#include "Computer.h"
 
 int main() {
     Human player1("Player1");
-    Human player2("Player2");
+    Computer player2("Computer");
     Referee referee;
 
     Player * winner = referee.refGame(&player1, &player2);
