59bc38dbb8908a325d0f23d0c89915eaae36f87c
Kurt Brice <a1887009@student.adelaide.edu.au>
2025-04-13 13:00:31 +0930

    4

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@
 #include "RecursiveBinarySearch.h"
 
 
-// int main() {
-//     std::vector<int> list1 = {1, 3, 5, 4, -5, 100, 7777, 2014};
-//     std::vector<int> list2 = {0, 3, 5, 4, -5, 100, 7777, 2014};
-//     RecursiveBinarySearch check;
-//     BubbleSort Bubble;
-//     Bubble.sort(list1);
-//     for (int i = 0; i < list1.size(); i++){
-//         std::cout << list1[i] << " ";
-//     }
-//     std::cout << check.search(list1, 1) << "\n";
-//     QuickSort Quick;
-//     Quick.sort(list2);
-//     for (int i = 0; i < list2.size(); i++){
-//         std::cout << list2[i] << " ";
-//     }
+int main() {
+    // std::vector<int> list1 = {1, 3, 5, 4, -5, 100, 7777, 2014};
+    // std::vector<int> list2 = {0, 3, 5, 4, -5, 100, 7777, 2014};
+    // RecursiveBinarySearch check;
+    // BubbleSort Bubble;
+    // Bubble.sort(list1);
+    // for (int i = 0; i < list1.size(); i++){
+    //     std::cout << list1[i] << " ";
+    // }
+    // std::cout << check.search(list1, 1) << "\n";
+    // QuickSort Quick;
+    // Quick.sort(list2);
+    // for (int i = 0; i < list2.size(); i++){
+    //     std::cout << list2[i] << " ";
+    // }
 
-//     std::cout << check.search(list2, 1) << "\n";
+    // std::cout << check.search(list2, 1) << "\n";
 
-//     return 0;
-// }
+    return 0;
+}
